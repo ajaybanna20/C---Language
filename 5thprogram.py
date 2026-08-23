fdec20d9b80f4305762c5a01a6c17062de2c7a1f
AJAY SINGH <ajayshekhawat990@gmail.com>
2026-08-23 23:59:38 +0530

    Add files via upload

---
+++ 5thprogram.py
@@ -0,0 +1,11 @@
+#logical Operator
+a = 50
+b = 10
+print (not False)
+print (not (a > b))
+
+valu1 = True
+valu2 = False
+print ("AND operator:",valu1 and valu2 )
+
+print ("OR operator:", valu1 or valu2)
