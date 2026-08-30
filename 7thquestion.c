5d36a8b63989fa92686c0ee64a43fcd13eaacd57
AJAY SINGH <ajayshekhawat990@gmail.com>
2026-08-30 15:54:01 +0530

    Add files via upload

---
+++ 7thquestion.c
@@ -0,0 +1,27 @@
+/* Q)write a c program input character and check character is vowel and consonant ?
+vowel :a,e,i,o,u consoant : */
+#include <stdio.h>
+int main () {
+	char ch;
+	printf("enter a character: ");
+	scanf("%c",&ch);
+	if (ch =='a') {
+		printf("vowel");
+	}
+	else if (ch == 'e') {
+		printf("vowel");
+	}
+	else if (ch == 'i') {
+		printf("vowel");
+	} else if (ch == 'o') {
+		printf("vowel");
+	}
+	else if (ch == 'u') {
+		printf("vowel");
+	}
+	else {
+		printf("consonent");
+	}
+printf("\nrest of the work");
+return 0;
+}
