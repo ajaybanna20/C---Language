74044b8a16c20c8ea190bc2cc42180fdd5fd82b0
AJAY SINGH <ajayshekhawat990@gmail.com>
2026-09-03 13:03:22 +0530

    Add files via upload

---
+++ 9thquestion.c
@@ -0,0 +1,42 @@
+/* write a c program input character and check character is q,w,e,r,t,y,u,i,o,p : upper line
+otherwise :input is not matched */
+#include <stdio.h>
+int main () {
+	char ch;
+	printf("enter a character: ");
+	scanf("%c",&ch);
+	if (ch =='q') {
+		printf("upper line");
+	}
+	else if (ch == 'w') {
+		printf("upper line");
+	}
+	else if (ch == 'e') {
+		printf("upper line");
+	} 
+	else if (ch == 'r') {
+		printf("upper line");
+	}
+	else if (ch == 't') {
+		printf("upper line");
+	}
+	else if (ch == 'y') {
+		printf("upper line");
+	}
+	else if (ch == 'u') {
+		printf("upper line");
+	}
+	else if (ch == 'i') {
+		printf("upper line");
+	}
+	else if (ch == 'o') {
+		printf("upper line");
+	}
+	else if (ch == 'p') {
+		printf("upper line");
+	}
+	else {
+		printf("input is not matched");
+	}
+return 0;
+}
